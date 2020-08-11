d5cf8b75de17f776f91fd9bdb958af010ea5e521
Travis Dick <travis.barry.dick@gmail.com>
2020-08-11 15:23:23 -0400

    Tile coder now includes min and max values for each variable

--- src/sirq_tilecoder.jl
+++ src/sirq_tilecoder.jl
@@ -2,17 +2,31 @@ import SparseArrays
 
 struct SIRQTileCoder
     S_bins::Int
+    S_min::Float64
+    S_max::Float64
+    
     I_bins::Int
+    I_min::Float64
+    I_max::Float64
+    
     R_bins::Int
+    R_min::Float64
+    R_max::Float64
+    
     Q_bins::Int
+    Q_min::Float64
+    Q_max::Float64
+    
     max_I_bins::Int
+    max_I_min::Float64
+    max_I_max::Float64
 end
 
 num_features(Φ::SIRQTileCoder) =
     Φ.S_bins * Φ.I_bins * Φ.R_bins * Φ.Q_bins * Φ.max_I_bins
 
 get_bin(min_val, max_val, nbins, x) =
-    min(floor(Int, (x - min_val) / (max_val - min_val) * nbins) + 1, nbins)
+    clamp(floor(Int, (x - min_val) / (max_val - min_val) * nbins) + 1, 1, nbins)
 
 function get_features(Φ::SIRQTileCoder, s::SIRQState)
     linear_indices = LinearIndices((
@@ -22,13 +36,16 @@ function get_features(Φ::SIRQTileCoder, s::SIRQState)
         1:Φ.Q_bins,
         1:Φ.max_I_bins,
     ))
+
     n = population_size(s)
+    
     tile_coord = CartesianIndex(
-        get_bin(0, n, Φ.S_bins, s.S),
-        get_bin(0, n, Φ.I_bins, s.I),
-        get_bin(0, n, Φ.R_bins, s.R),
-        get_bin(0, n, Φ.Q_bins, s.Q),
-        get_bin(0, n, Φ.max_I_bins, s.max_I),
+        get_bin(Φ.S_min, Φ.S_max, Φ.S_bins, s.S),
+        get_bin(Φ.I_min, Φ.I_max, Φ.I_bins, s.I),
+        get_bin(Φ.R_min, Φ.R_max, Φ.R_bins, s.R),
+        get_bin(Φ.Q_min, Φ.Q_max, Φ.Q_bins, s.Q),
+        get_bin(Φ.max_I_min, Φ.max_I_max, Φ.max_I_bins, s.max_I),
     )
+    
     SparseArrays.SparseVector(num_features(Φ), [linear_indices[tile_coord]], [1.0])
 end
